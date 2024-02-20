3b6881078601f877f68915832d94daccff420787
Owale128 <Christian.buaka@hotmail.com>
2024-02-20 20:57:53 +0100

    Created a pokemonApp description page and did the same thing as previous page

---
+++ src/pokemonApp.ts
@@ -0,0 +1,41 @@
+
+import { saveLanguage, getLanguage } from "./main";
+
+const swedishElements = document.querySelectorAll('.swedish');
+const englishElements = document.querySelectorAll('.english');
+const languageSelect = document.getElementById('language-select') as HTMLSelectElement;
+
+languageSelect.addEventListener('change', () => {
+    const selectedLanguage = languageSelect.value;
+    saveLanguage(selectedLanguage);
+    updateLanguage(selectedLanguage);
+});
+
+const updateLanguage = (language: string) => {
+    if (language === 'en') {
+        swedishElements.forEach(element => {
+            element.classList.add('none');
+        });
+        englishElements.forEach(element => {
+            element.classList.remove('none');
+        });
+    } else {
+        englishElements.forEach(element => {
+            element.classList.add('none');
+        });
+        swedishElements.forEach(element => {
+            element.classList.remove('none');
+        });
+    }
+};
+
+const activateSavedLanguage = () => {
+    const savedLanguage = getLanguage();
+    if (savedLanguage) {
+        updateLanguage(savedLanguage);
+    }
+};
+
+window.onload = () => {
+    activateSavedLanguage();
+};
