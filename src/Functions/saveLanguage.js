3e6315f6e691dea6551105ed11bfe8cbea4ba93b
Owale128 <Christian.buaka@hotmail.com>
2024-02-22 12:09:45 +0100

    Changed from localStorage to SessionStorage

--- src/Functions/saveLanguage.js
+++ src/Functions/saveLanguage.js
@@ -2,12 +2,12 @@
 // Object.defineProperty(exports, "__esModule", { value: true });
 // exports.getLanguage = exports.saveLanguage = void 0;
 var saveLanguage = function (language) {
-    localStorage.setItem('language', language);
+    sessionStorage.setItem('language', language);
 };
 const _saveLanguage = saveLanguage;
 export { _saveLanguage as saveLanguage };
 var getLanguage = function () {
-    return localStorage.getItem('language');
+    return sessionStorage.getItem('language');
 };
 const _getLanguage = getLanguage;
 export { _getLanguage as getLanguage };
