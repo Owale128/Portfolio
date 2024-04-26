804619669b18d7c947d62f7b802bd57f243221be
Owale128 <Christian.buaka@hotmail.com>
2024-04-26 18:00:00 +0200

    minor fix

--- src/TS/JS/searchMovieApp.js
+++ src/TS/JS/searchMovieApp.js
@@ -1,5 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+// Object.defineProperty(exports, "__esModule", { value: true });
 var saveLanguage_1 = require("../../Functions/saveLanguage");
 var swedishElements = document.querySelectorAll('.swedish');
 var englishElements = document.querySelectorAll('.english');
